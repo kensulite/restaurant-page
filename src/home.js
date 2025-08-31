ddd152be91b49737b9c619850450e0414e46132e
Jean Alvarez <225872103+kensulite@users.noreply.github.com>
2025-08-31 10:25:12 -0500

    Add home headline

--- src/home.js
+++ src/home.js
@@ -2,17 +2,28 @@ import banner from "./assets/Site-background-dark.png"
 
 const createHomepage = () => {
     const main = document.createElement("main");
+    main.classList.add("home");
     const bannerImage = createBannerImage();
-    main.append(bannerImage);
+    const headline = createHeadline();
+    main.append(bannerImage, headline);
     return main;
 }
 
 const createBannerImage = () => {
     const image = document.createElement("img");
+    image.classList.add("banner");
     image.src = banner;
     image.setAttribute("width", "1280");
     image.setAttribute("height", "292");
     return image;
+}
+
+const createHeadline = () => {
+    const heading = document.createElement("h2");
+    const firstPart = document.createTextNode("Where Every Drink Tells a Story");
+    const secondPart = document.createTextNode("Welcome to VA-11 HALL-A");
+    heading.append(firstPart, document.createElement("br"), secondPart);
+    return heading;
 }
 
 export { createHomepage }
