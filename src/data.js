21b3eca99c2bb047446f87d44ab6b93c5876d92c
Jean Alvarez <225872103+kensulite@users.noreply.github.com>
2025-09-11 13:59:16 -0500

    Add menu data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import BadTouch from "./assets/badtouch.png";
+import BleedingJane from "./assets/bleedingjane.png";
+import BlueFairy from "./assets/bluefairy.png";
+import CobaltVelvet from "./assets/cobaltvelvet.png";
+import FluffyDream from "./assets/fluffydream.png";
 
 const menu = [
     {
@@ -9,7 +14,8 @@ const menu = [
             "Powdered Delta": 2,
             "Flanergide": 2,
             "Karmotrine": 4
-        }
+        },
+        image: BadTouch
     },
     {
         name: "Bleeding Jane",
@@ -19,11 +25,39 @@ const menu = [
             "Bronson Extract": 1,
             "Powdered Delta": 3,
             "Flanergide": 3
-        }
+        },
+        image: BleedingJane
     },
     {
         name: "Blue Fairy",
-
+        description: "One of these will make all your teeth turn blue. Hope you brushed them well.",
+        properties: ["Sweet", "Girly", "Soft"],
+        ingredients: {
+            "Adelhyde": 4,
+            "Flanergide": 1,
+        },
+        image: BlueFairy
+    },
+    {
+        name: "Cobalt Velvet",
+        description: "It's like champaigne *sic* served on* sic* a cup that had a bit of cola left.",
+        properties: ["Bubbly", "Classy", "Burning"],
+        ingredients: {
+            "Adelhyde": 2,
+            "Flanergide": 3,
+            "Karmotrine": 5
+        },
+        image: CobaltVelvet
+    },
+    {
+        name: "Fluffy Dream",
+        description: "A couple of these will make your tongue feel velvet-y. More of them and you'll be sleeping soundly.",
+        properties: ["Sour", "Girly", "Soft"],
+        ingredients: {
+            "Adelhyde": 3,
+            "Powdered Delta": 3
+        },
+        image: FluffyDream
     }
 ]
 
